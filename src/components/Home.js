521e4dc984bff773585baef8d76df81cda95e54f
GaneshRoyal07 <kondetiganesh43@gmail.com>
2026-03-25 17:39:44 +0530

    using api

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,19 @@
-
-
-
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./Home.css";
 
 function Home() {
   const [city, setCity] = useState("");
   const navigate = useNavigate();
 
   const cities = [
-  "Bengaluru","Hyderabad","Chennai","Mumbai","Delhi","Goa",
-  "Kolkata","Pune","Ahmedabad","Jaipur","Lucknow",
-  "Surat","Kanpur","Nagpur","Indore","Bhopal",
-  "Visakhapatnam","Patna","Vadodara","Coimbatore"
-];
+    "Bengaluru","Hyderabad","Chennai","Koduru",
+    "Tirupati","Mumbai","Delhi","Goa",
+    "Kolkata","Pune","Ahmedabad","Jaipur","Lucknow",
+    "Surat","Kanpur","Nagpur","Indore","Bhopal",
+    "Visakhapatnam","Patna","Vadodara","Coimbatore"
+  ];
+
   const handleSubmit = () => {
     if (city) {
       navigate(`/weather/${city}`);
@@ -23,16 +23,18 @@ function Home() {
   };
 
   return (
-    <div style={styles.container}>
-      <div style={styles.card}>
-        <h1 style={styles.title}>🌦 Weather App</h1>
+    <div className="home-container">
+      <div className="home-card">
+        <h1 className="home-title">🌦 Weather App</h1>
 
-        <p style={styles.subtitle}>Select a city to check weather</p>
+        <p className="home-subtitle">
+          Select a city to check weather
+        </p>
 
         <select
           value={city}
           onChange={(e) => setCity(e.target.value)}
-          style={styles.select}
+          className="home-select"
         >
           <option value="">-- Select City --</option>
           {cities.map((c, i) => (
@@ -40,59 +42,12 @@ function Home() {
           ))}
         </select>
 
-        <button onClick={handleSubmit} style={styles.button}>
+        <button onClick={handleSubmit} className="home-button">
           Get Weather
         </button>
       </div>
     </div>
   );
 }
-
-const styles = {
-  container: {
-    minHeight: "100vh",
-    paddingTop: "50px",   
-    paddingBottom: "100px", 
-    background: "linear-gradient(to right, #667eea, #764ba2)",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  card: {
-    background: "#fff",
-    padding: "40px",
-    borderRadius: "20px",
-    textAlign: "center",
-    width: "350px",
-    boxShadow: "0 10px 30px rgba(0,0,0,0.2)",
-  },
-  title: {
-    fontSize: "28px",
-    fontWeight: "bold",
-  },
-  subtitle: {
-    color: "#666",
-    marginBottom: "20px",
-  },
-  select: {
-    width: "100%",
-    padding: "10px",
-    marginBottom: "20px",
-    borderRadius: "10px",
-    position: "relative",
-    zIndex: 1,
-
-  },
-  button: {
-    width: "100%",
-    padding: "12px",
-    border: "none",
-    borderRadius: "10px",
-    background: "#667eea",
-    color: "#fff",
-    fontSize: "16px",
-    cursor: "pointer",
-  },
-};
 
 export default Home;
